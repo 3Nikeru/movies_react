8c6ae015a60de0cc94564b854252f2a01b14f4ec
Alina Nikeru <alina.nikeru@gmail.com>
2022-12-16 22:10:48 +0200

    pages

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 // import { Provider } from 'react-redux';
 
 // import store from './store/configureStore'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     {/* <Provider store={store}> */}
       <App/>
     {/* </Provider> */}
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
